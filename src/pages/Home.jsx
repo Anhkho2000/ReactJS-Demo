8f8f573b80a53eee63d12d168e0cdcc0efde9475
Anhkho2000 <94821877+Anhkho2000@users.noreply.github.com>
2022-03-07 18:42:59 +0700

    Update-to

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 // import Components
 import Helmet from '../components/Helmet'
@@ -6,11 +7,12 @@ import HeroSlider from '../components/HeroSlider'
 import Section, { SectionTitle, SectionBody } from '../components/Section'
 import PolicyCard from '../components/PolicyCard'
 import Grid from '../components/Grid'
-
+import ProductCard from '../components/ProductCard'
 
 // fake data
 import heroSliderData from '../assets/fake-data/hero-slider'
 import policy from '../assets/fake-data/policy'
+import productData from '../assets/fake-data/products'
 
 const Home = () => {
         return (
@@ -36,17 +38,47 @@ const Home = () => {
                                         >
 
                                                 {
-                                                        policy.map((item, index) => <PolicyCard
-                                                                key={index}
-                                                                name={item.name}
-                                                                description={item.description}
-                                                                icon={item.icon}
-                                                        />)
+                                                        policy.map((item, index) => <Link key={index} to="/policy">
+                                                                <PolicyCard
+                                                                        name={item.name}
+                                                                        description={item.description}
+                                                                        icon={item.icon}
+                                                                />
+                                                        </Link>)
                                                 }
                                         </Grid>
                                 </SectionBody>
                         </Section>
                         {/* end policy section */}
+
+                        {/* best selling section */}
+                        <Section>
+                                <SectionTitle>
+                                        Top sản phẩm bán chạy trong tuần
+                                </SectionTitle>
+                                <SectionBody>
+                                        <Grid
+                                                col={4}
+                                                mdCol={2}
+                                                smCol={1}
+                                                gap={20}
+                                        >
+                                                {
+                                                        productData.getProducts(4).map((item,index)=>(
+                                                                <ProductCard
+                                                                key={index}
+                                                                img01={item.image01}
+                                                                img02={item.image02}
+                                                                name={item.title}
+                                                                price={item.price}
+                                                                slug={item.slug}
+                                                                />
+                                                        ))
+                                                }
+                                        </Grid>
+                                </SectionBody>
+                        </Section>
+                        {/* end best selling section */}
                 </Helmet>
         )
 }
